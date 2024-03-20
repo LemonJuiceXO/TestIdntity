3cabbc24cd19481f59cf0a0040a57bf5169d4b73
radoin cr <radoin082@gmail.com>
2024-03-20 03:12:55 +0100

    button logout >> link text error 'ViewBag'

--- GLAB.Test/obj/Debug/net8.0/GLAB.Test.AssemblyInfo.cs
+++ GLAB.Test/obj/Debug/net8.0/GLAB.Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GLAB.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2cf67c2a970a9ad77348baf8ceb0713aa799f5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+555fe59ac64608706a5ca2942e026e858f6100a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("GLAB.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GLAB.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
